bb3184f874a61ce2696439435c3e2b65efeaed3d
Harshita Sharma <harshitasharma1973@gmail.com>
2021-06-14 21:34:00 +0530

    Updates

---
+++ precache-manifest.0b75997cae0d72164b6e02d693f332dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6625d6b9478c482a3e7ce0d08e173f",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/todoapp/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "a5b1f0ccffa149765232",
+    "url": "/todoapp/static/js/main.0fba3adb.chunk.js"
+  },
+  {
+    "revision": "0626debe875f4fdce225",
+    "url": "/todoapp/static/js/runtime-main.3e13f74e.js"
+  }
+]);
